12a9727a31fae5db804432587ee68cda874f98df
MusaPolka <75001112+MusaPolka@users.noreply.github.com>
2021-02-26 10:49:22 +0600

    Add files via upload

---
+++ p-18-44.kz/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$x = 70;
+$y = 35;
+$z = $x / $y;
+echo $z. '<br>';
+
+$q = 10;
+$num = $q * $z;
+
+echo $num. '<br>';
+
+$age = 17;
+echo 'Менын жасым '.$age . '<br>';
+
+$str = "qwerty";
+
+echo $str[0] . ' ' . $str[1] . ' ' . $str[2] . '<br>';
+
+
+    ?>
+</body>
+</html>
